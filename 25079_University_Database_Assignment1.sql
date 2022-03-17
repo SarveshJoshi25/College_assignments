25a618376e4dd630bcf1b114bd40545e7bdc096f
Sarvesh S Joshi <mynameissarveshjoshi@gmail.com>
2022-03-17 11:56:24 +0530

    Added Section Table

--- 25079_University_Database_Assignment1.sql
+++ 25079_University_Database_Assignment1.sql
@@ -6,3 +6,6 @@ create table instructor (instructor_id int primary key, instructor_name varchar(
 create table course(course_id varchar(255) primary key, course_title varchar(255) not null, credits int not null, department_name varchar(255), foreign key(department_name) references department(department_name));
 create table classroom (building varchar(255), room_number varchar(255), capacity int default 60, primary key(building, room_number));
 create table time_slot(time_slot_id varchar(255) primary key, day varchar(255) not null, start_time time not null, end_time time not null);
+
+create table section(section_id varchar(255), semester varchar(255), year varchar(255), course_id varchar(255), foreign key(course_id) references course(course_id), building varchar(255), room_number varchar(255), foreign key(building, room_number) references classroom(building,room_number), time_slot_id varchar(255), foreign key(time_slot_id) references time_slot(time_slot_id), primary key(course_id, section_id, semester, year));
+
