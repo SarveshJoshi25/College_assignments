1b0a496b02152d5881ec6e1c9c36503bff0058ad
Sarvesh S Joshi <mynameissarveshjoshi@gmail.com>
2022-03-23 12:02:51 +0530

    Update MouseInterfacing.cpp

--- MouseInterfacing.cpp
+++ MouseInterfacing.cpp
@@ -3,14 +3,48 @@
 using namespace std;
 double x1, y1;
 int Xsize, YSize;
+double radius = 45, XCenter, YCenter;
+double xnew, ynew;
 
 void myInit(){
 	glClearColor(1.0,1.0,1.0,0);
 	glColor3f(0.0,0.0,0.0);
 	glPointSize(2.0);
 
-	gluOrtho2D(320 , -320 , 240 , -240);
+	gluOrtho2D(-320 , 320 , -240 , 240);
 }
+
+bool symmetry(double x, double y, double xc, double yc){
+	glVertex2d(x + xc, y + yc);
+	glVertex2d(-x + xc, y + yc);
+	glVertex2d(x + xc, -y + yc);
+	glVertex2d(-x + xc, -y + yc);
+	glVertex2d(y + yc, x + xc);
+	glVertex2d(-y + yc, x + xc);
+	glVertex2d(y + yc, -x + xc);
+	glVertex2d(-y + yc, -x + xc);
+}
+
+
+
+bool bresenhamCircleDrawing(){
+	double x = 0, y = radius;
+	double sum = 3 - 2 * radius;
+	
+	while(x < y){
+		if(sum <= 0){
+			sum += 4 * x + 6;
+			x++; 	
+		}
+		else{
+			sum += 4 * x - 4 * y  + 10;
+			x++; y--;
+		}
+		symmetry(x, y, XCenter, YCenter);
+		
+	}
+}
+
 
 void myMouse(int button, int state, int x, int y){
 	if(state== GLUT_DOWN){
@@ -18,7 +52,10 @@ void myMouse(int button, int state, int x, int y){
 			x1=x;
 			y1=y;
 			glBegin(GL_POINTS);
-			glVertex2f((x-320) * -1 , (y-240));
+			XCenter = (x - 320) * -1; YCenter = y - 240;
+			cout << XCenter << endl << YCenter;
+			bresenhamCircleDrawing();
+			//glVertex2f((x-320) * -1 , (y-240));
 			glEnd();
 			glFlush();
 		}
@@ -33,8 +70,6 @@ void myMouse(int button, int state, int x, int y){
 
 
 void MyDisplay(){
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glBegin(GL_LINES);
 
